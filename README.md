f2a4aec9f0e297a88e8c48b542e7e5e74a3cebaa
Nanyuss <105124579+Nanyuss@users.noreply.github.com>
2024-06-16 13:24:53 -0300

    Adicionando os arquivos e configurando o repositorio

---
+++ README.md
@@ -0,0 +1,56 @@
+<h1 align="center">✨Nayul Discord Bot✨<h1>
+
+<p align="center">
+<a><img src="https://img.shields.io/github/languages/top/Nanyuss/NayulDiscordBot"></a>
+<a href="https://github.com/Nanyuss/NayulDiscordBot/commits/master"><img src="https://img.shields.io/github/last-commit/Nanyuss/NayulDiscordBot"></a>
+<a href="LICENSE"><img src="https://img.shields.io/github/license/Nanyuss/NayulDiscordBot%20
+"></a>
+</p>
+
+# 📋Sobre o projeto
+
+Nayul é um bot para Discord feito em Python que eu estava criando com o objetivo de ser um bot de economia e utilitários. Mas por falta de tempo para desenvolve-la e ter novas ideias, decidi desativá-la por tempo indeterminado enquanto envio aos poucos atualizações no repositório dela e quem sabe alguém contribuir.
+
+## 🛠 Requisitos do Sistema
+
+- Python 3.9, 3.10 ou 3.11
+- GIT
+
+## 🤝 Contribuição
+
+Contribuições de todos os tipos são bem-vindas! Desde pequenas correções de bugs até grandes novos recursos.
+
+### Como Contribuir 🚀
+
+1. **Fork** o repositório e clone-o localmente:
+    ```sh
+    git clone https://github.com/<seu_usuario>/NayulDiscordBot.git
+    cd NayulDiscord
+    ```
+
+2. Configure o arquivo `.env`:
+    1. Mude o nome do arquivo `exemplo.env` para `.env`.
+
+    2. Abra o arquivo `.env` e adicione o seu token de aplicação do Discord e ids dos donos:
+        ```env
+        TOKEN="seu_token_aqui"
+        OWNER_IDS="1234"
+        ```
+
+3. Instale as dependências:
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+4. Faça suas alterações e commit.
+
+5. Faça um push para seu repositório com `git push`
+
+6. Crie um novo **Pull Request** no GitHub enviando as alterações para o repositório principal, detalhando as mudanças.
+
+
+## 🔗 Links Úteis
+
+- [Documentação do Discord.py](https://discordpy.readthedocs.io/en/stable/)
+- [Documentação do Python](https://docs.python.org/3.11/)
+- [Developer Portal do Discord](https://discord.com/developers/)
---
+++ main.py
@@ -0,0 +1,73 @@
+import os
+import asyncio
+import logging
+from decouple import config
+
+import discord
+from discord.ext import commands
+
+#Configurações básicas do logging.
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+#Classe principal da aplicação.
+class MyBot(commands.Bot):
+	def __init__(self):
+		super().__init__(
+			intents=discord.Intents(
+				messages=True,
+				guilds=True,
+				members=True,
+				message_content=True,
+				guild_messages=True,
+				dm_messages=True,
+				webhooks=True,
+				emojis_and_stickers=True,
+				guild_reactions=True,
+				guild_typing=True
+			), #Definindo as intents que serão ativadas.
+			allowed_mentions=discord.AllowedMentions(
+				everyone=False,
+				roles=False
+			), #Desativando o ping das menções de cargos e everyone nas mensagens da aplicação.
+			command_prefix=",,", #Prefixo da aplicação.
+			help_command=None #Desativando o comando padrão de ajuda.
+		)
+		self.owner_ids = set()
+		
+		#Adicionando os IDs dos proprietários definidos no .env.
+		for owner in config("OWNER_IDS").split("||"):
+			if not owner:
+				continue
+			
+			try:
+				self.owner_ids.add(int(owner))
+			except ValueError:
+				log.warning(f"Owner_ID Inválido: {owner}")
+		
+	#Função que carrega todas as extensões (cogs).
+	async def load_cogs(self, path: str):
+		for root, _, files in os.walk(path):
+			for file in files:
+				if file.endswith(".py"):
+					try:
+						await self.load_extension(os.path.join(root, file)[:-3].replace(os.path.sep, "."))
+						log.warning(f"✅Carregado {f!r} de {root[5:]!r}.")
+					except Exception:
+						log.error(f"❌Falha ao carregar {f!r} de {root[5:]!r}.", exc_info=True)
+	
+	#Evento de configuração chamado enquanto a aplicação está se iniciando.
+	async def setup_hook(self):
+		await self.load_cogs("cogs")
+	
+	#Evento chamado quando a aplicação está completamente ligada
+	async def on_ready(self):
+		print(f"Conectada como: {self.user} (ID: {self.user.id})")
+
+#Função principal para inicializar e rodar a aplicação.	
+async def main():
+	async with (bot := MyBot()):
+		await bot.start(config("TOKEN"))
+
+if __name__ == "__main__":
+	asyncio.run(main())
